73375be4c0a7efe17730b09c5782a98737e7ccb5
vinicius.shk <vinicius.shk@gmail.com>
2023-08-24 13:43:46 -0300

    feat(ProjCreation):
    Create the maven project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Purchase Orders
+
+RESTfull API to manage purchase orders. This service includes:
+
+- Postgres SQL Database
+- JWT Authentication
+- External API consuption to generate products
+- Confirmation messages to both parties of the order
+
---
+++ src/test/java/com/adaavanade/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaavanade.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
